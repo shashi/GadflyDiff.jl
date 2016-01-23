e44146f1b2e133806aa76526518494b0151f2ae0
Shashi Gowda <shashigowda91@gmail.com>
2016-01-23 23:58:37 +0530

    Moved Patchwork logic to this package

--- src/GadflyDiff.jl
+++ src/GadflyDiff.jl
@@ -1,5 +1,22 @@
 module GadflyDiff
 
-# package code goes here
+using ComposeDiff
+using Reactive
+using Compose
+using Gadfly
+
+import Base: writemime
+import IJulia
+
+
+function writemime(io::IO, m::MIME"text/html", ctx::Signal{Gadfly.Plot})
+    writemime(io, m, map(c -> draw(
+        Patchable(
+            Compose.default_graphic_width,
+            Compose.default_graphic_height
+        ), c), ctx))
+end
+
+IJulia.metadata(::Reactive.Signal{Plot}) = Dict()
 
 end # module
